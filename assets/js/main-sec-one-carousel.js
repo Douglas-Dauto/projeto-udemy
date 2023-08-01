cab8b37fd4c40c9e27c7e6374860e3bfd5bd643a
Douglas-Dauto <dautodouglasdauto@gmail.com>
2023-08-01 16:44:51 -0300

    Modificado os arquivos script.js e _main.scss

--- assets/js/main-sec-one-carousel.js
+++ assets/js/main-sec-one-carousel.js
@@ -2,26 +2,50 @@
 const contentCarousel = window.document.getElementsByClassName('main-sec-one-content')[0];
 const carouselPrevious = window.document.getElementsByClassName('main-sec-one-previous')[0];
 const carouselNext = window.document.getElementsByClassName('main-sec-one-next')[0];
-const elementsCarousel = ['<img class="carousel-img" src="assets/img/carrosselOne.jpg" alt="Certificado">', '<img class="carousel-img" src="assets/img/carrosselTwo.jpg" alt="Boneco subindo degraus">', '<img class="carousel-img" src="assets/img/carrosselThree.png" alt="Pessoa sorrindo">'];
+const elementsCarousel = [
+`<div class="carousel-content-all">
+    <img class="carousel-img" src="assets/img/carrosselOne.jpg" alt="Certificado">
+
+    <div class="carousel-content">
+        <h1>Prepare-se para sua certificação de TI</h1>
+        <p>Explore um futuro em TI. Comece a estudar para tirar a certificação AWS, CompTIA A+ e muito mais.</p>
+    </div>
+</div>`, 
+`<div class="carousel-content-all">
+    <img class="carousel-img" src="assets/img/carrosselTwo.jpg" alt="Boneco subindo degraus">
+
+    <div class="carousel-content">
+        <h1>Devagar e sempre</h1>
+        <p>Tente estudar por 5 a 10 minutos por dia. Continue seu curso e alcance todo o seu potencial.</p>
+    </div>
+</div>`, 
+`<div class="carousel-content-all">
+    <img class="carousel-img" src="assets/img/carrosselThree.png" alt="Pessoa sorrindo">
+
+    <div class="carousel-content">
+        <h1>O aprendizado vem até você</h1>
+        <p>A promoção termina hoje. Alcance um mundo de conhecimento — em casa ou na rua. Cursos a partir de R$22,90 cada.</p>
+    </div>
+</div>`];
 let intervalNextImageData;
 
 for(let i = 0; i < elementsCarousel.length; i++) {
     contentCarousel.innerHTML += elementsCarousel[i];
 }
 
-const carouselImg = window.document.getElementsByClassName('carousel-img');
+const carouselContentAll = window.document.getElementsByClassName('carousel-content-all');
 
 carouselPrevious.addEventListener('click', previousElement);
 carouselNext.addEventListener('click', nextElement);
 
 function previousElement() {
-    contentCarousel.insertBefore(carouselImg[carouselImg.length -1], carouselImg[0]);
+    contentCarousel.insertBefore(carouselContentAll[carouselContentAll.length -1], carouselContentAll[0]);
     clearInterval(intervalNextImageData);
     intervalNextImage();
 }
 
 function nextElement() {
-    contentCarousel.appendChild(carouselImg[0]);
+    contentCarousel.appendChild(carouselContentAll[0]);
     clearInterval(intervalNextImageData);
     intervalNextImage();
 }
