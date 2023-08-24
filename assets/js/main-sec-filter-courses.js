2d32a457f86e3c017ee6ada75e575d304721cdbf
Douglas-Dauto <dautodouglasdauto@gmail.com>
2023-08-24 19:14:32 -0300

    Adicionado load a pesquisa por cursos

--- assets/js/main-sec-filter-courses.js
+++ assets/js/main-sec-filter-courses.js
@@ -7,7 +7,7 @@ import {containerSelectionMouseOff} from './container-header-two-container-selec
     const sectionFilter = window.document.getElementsByClassName('main-sec-filter-courses')[0];
     const main = window.document.getElementsByTagName('main')[0];
     const courses = coursesArray;
-    let countResult = 0;
+    let countResult = 0, textInputSearchValuePrevious = '';
 
     setInterval(() => {
         fillButtonSearch();
@@ -29,136 +29,145 @@ import {containerSelectionMouseOff} from './container-header-two-container-selec
     }
 
     inputSearch.addEventListener('keypress', (e) => {
-        if(e.keyCode === 13 && inputSearch.value !== '') {
+        if(e.keyCode === 13) {
             injectSection();
         }
     });
     
     function injectSection() {
-        countResult = 0;
+        if(inputSearch.value !== '' && inputSearch.value !== textInputSearchValuePrevious) {
+            countResult = 0;
 
-        for(let i in courses) {
-            if(courses[i].title.toLowerCase().includes(String(inputSearch.value.toLowerCase()))) {
-                countResult++;
+            for(let i in courses) {
+                if(courses[i].title.toLowerCase().includes(String(inputSearch.value.toLowerCase()))) {
+                    countResult++;
+                }
             }
-        }
 
-        main.innerHTML = '';
-        main.innerHTML = `<section class="main-sec-filter-courses">
-        <h1>${countResult} resultados para “${inputSearch.value}”</h1>
-    
-        <div class="main-sec-filter-courses__buttons">
-            <div>
-                <button>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-filter" viewBox="0 0 16 16">
-                    <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
-                  </svg>Filtro
-                </button>
+            main.innerHTML = '';
+            main.innerHTML = '<img src="assets/img/icon/arrow-clockwise.svg" class="load-arrow" alt="Load">';
+
+            setTimeout(() => {
+                main.innerHTML = '';
+                main.innerHTML = `<section class="main-sec-filter-courses">
+                <h1>${countResult} resultados para “${inputSearch.value}”</h1>
             
-                <div>
-                    <select>
-                        <option value="">Mais relevantes</option>
-                        <option value="">Mais vendidos</option>
-                        <option value="">Classificação mais alta</option>
-                    </select>
-                </div>
-            </div>
-    
-            <p>${countResult} resultados</p>
-        </div>
-    
-        <div class="main-sec-filter-courses__courses-and-filter">
-            <div>
-                <div>
-                    <h4>Classificalções</h4>
-    
+                <div class="main-sec-filter-courses__buttons">
                     <div>
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-filter" viewBox="0 0 16 16">
+                            <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
+                        </svg>Filtro
+                        </button>
+                    
                         <div>
-                            <input type="radio" name="classification" id="four-five">
-                            <label for="four-five">4,5 e Acima</label>
-                        </div>
-    
-                        <div>
-                            <input type="radio" name="classification" id="four-zero">
-                            <label for="four-zero">4,0 e Acima</label>
-                        </div>
-    
-                        <div>
-                            <input type="radio" name="classification" id="three-five">
-                            <label for="three-five">3,5 e Acima</label>
-                        </div>
-    
-                        <div>
-                            <input type="radio" name="classification" id="three-zero">
-                            <label for="three-zero">3,0 e Acima</label>
+                            <select>
+                                <option value="">Mais relevantes</option>
+                                <option value="">Mais vendidos</option>
+                                <option value="">Classificação mais alta</option>
+                            </select>
                         </div>
                     </div>
+            
+                    <p>${countResult} resultados</p>
                 </div>
-            </div>
-    
-            <div class="container-courses-filter"></div>
-        </div>
-    </section>`;
+            
+                <div class="main-sec-filter-courses__courses-and-filter">
+                    <div>
+                        <div>
+                            <h4>Classificalções</h4>
+            
+                            <div>
+                                <div>
+                                    <input type="radio" name="classification" id="four-five">
+                                    <label for="four-five">4,5 e Acima</label>
+                                </div>
+            
+                                <div>
+                                    <input type="radio" name="classification" id="four-zero">
+                                    <label for="four-zero">4,0 e Acima</label>
+                                </div>
+            
+                                <div>
+                                    <input type="radio" name="classification" id="three-five">
+                                    <label for="three-five">3,5 e Acima</label>
+                                </div>
+            
+                                <div>
+                                    <input type="radio" name="classification" id="three-zero">
+                                    <label for="three-zero">3,0 e Acima</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            
+                    <div class="container-courses-filter"></div>
+                </div>
+            </section>`;
 
-    const containerCoursesFilter = window.document.getElementsByClassName('container-courses-filter')[0];
-    window.document.getElementsByClassName('main-sec-filter-courses')[0].addEventListener('mouseenter', containerSelectionMouseOff);
+            const containerCoursesFilter = window.document.getElementsByClassName('container-courses-filter')[0];
+            window.document.getElementsByClassName('main-sec-filter-courses')[0].addEventListener('mouseenter', containerSelectionMouseOff);
 
-    for(let i in courses) {
-        if(courses[i].title.toLowerCase().includes(String(inputSearch.value.toLowerCase()))) {
-            let textClassificationClass;
+            for(let i in courses) {
+                if(courses[i].title.toLowerCase().includes(String(inputSearch.value.toLowerCase()))) {
+                    let textClassificationClass;
 
-            if(courses[i].classification === 'Mais vendidos') {
-                textClassificationClass = 'classification--best-sellers';
-            } else if (courses[i].classification === 'Classificação mais alta') {
-                textClassificationClass = 'classification--highest-rating';
-            } else {
-                textClassificationClass = 'classification--none';
-            }
+                    if(courses[i].classification === 'Mais vendidos') {
+                        textClassificationClass = 'classification--best-sellers';
+                    } else if (courses[i].classification === 'Classificação mais alta') {
+                        textClassificationClass = 'classification--highest-rating';
+                    } else {
+                        textClassificationClass = 'classification--none';
+                    }
 
-            containerCoursesFilter.innerHTML += `<div>
-                                                    <div>
-                                                        <img src="${courses[i].image}" alt="">
-
-                                                        <div>
-                                                            <h3>${courses[i].title}</h3>
-                                                            <p>${courses[i].createdBy}</p>
-
+                    containerCoursesFilter.innerHTML += `<div>
                                                             <div>
-                                                                <p>${courses[i].starNote}</p>
-                                                                <p>(${courses[i].popularity})</p>
+                                                                <img src="${courses[i].image}" alt="">
+
+                                                                <div>
+                                                                    <h3>${courses[i].title}</h3>
+                                                                    <p>${courses[i].createdBy}</p>
+
+                                                                    <div>
+                                                                        <p>${courses[i].starNote}</p>
+                                                                        <p>(${courses[i].popularity})</p>
+                                                                    </div>
+
+                                                                    <p class="${textClassificationClass}">${courses[i].classification}</p>
+                                                                </div>
                                                             </div>
 
-                                                            <p class="${textClassificationClass}">${courses[i].classification}</p>
-                                                        </div>
-                                                    </div>
+                                                            <div>
+                                                                <p>R$${courses[i].valueMoney}</p>
+                                                            </div>
+                                                        </div>`;
+                }
+            }
 
-                                                    <div>
-                                                        <p>R$${courses[i].valueMoney}</p>
-                                                    </div>
-                                                </div>`;
-        }
-    }
+            const buttonFilter = window.document.querySelector('.main-sec-filter-courses__buttons button');
+            const containerFilter = window.document.querySelector('.main-sec-filter-courses__courses-and-filter > div:nth-child(1)');
+            let controlFilter = JSON.parse(localStorage.getItem('controlFilter'));
 
-    const buttonFilter = window.document.querySelector('.main-sec-filter-courses__buttons button');
-    const containerFilter = window.document.querySelector('.main-sec-filter-courses__courses-and-filter > div:nth-child(1)');
-    let controlFilter = JSON.parse(localStorage.getItem('controlFilter'));
-    
-    moveContainerFilter(false);
-    
-    buttonFilter.addEventListener('click', moveContainerFilter);
-    
-    function moveContainerFilter(value = true) {
-        if(value) {
-            controlFilter = !controlFilter;
-            JSON.stringify(controlFilter);
-            localStorage.setItem('controlFilter', controlFilter);
+            moveContainerFilter(false);
+
+            buttonFilter.addEventListener('click', moveContainerFilter);
+
+            function moveContainerFilter(value = true) {
+                if(value) {
+                    controlFilter = !controlFilter;
+                    JSON.stringify(controlFilter);
+                    localStorage.setItem('controlFilter', controlFilter);
+                }
+
+                if(controlFilter) {
+                    containerFilter.setAttribute('class', value === false?'courses-and-filter--hidden-transition-none':'courses-and-filter--hidden');
+                } else {
+                    containerFilter.removeAttribute('class', value === false?'courses-and-filter--hidden-transition-none':'courses-and-filter--hidden');
+                }
+                }
+            }, 2000);
+            
+            textInputSearchValuePrevious = inputSearch.value;
         }
-    
-        if(controlFilter) {
-            containerFilter.setAttribute('class', value === false?'courses-and-filter--hidden-transition-none':'courses-and-filter--hidden');
-        } else {
-            containerFilter.removeAttribute('class', value === false?'courses-and-filter--hidden-transition-none':'courses-and-filter--hidden');
-        }
-    }
     }
 })();
